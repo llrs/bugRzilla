675b0eeb919d09c734110ea7f5a9aee0efced31c
Lluís <lluis.revilla@gmail.com>
2021-02-13 12:12:19 +0100

    Change minor version to what is available on the website

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,9 @@ missing_host <- function(host) {
 missing_version <- function(version) {
     if (missing(version)) {
         ver <- R.Version()[c("major", "minor")]
+        if (ver$minor != "0.0") {
+            ver$minor <- "0.x"
+        }
         version <- paste0(ver, collapse = ".")
     }
     version
