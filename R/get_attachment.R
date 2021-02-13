e63efad7bbabe4c2bfed9d1ec5e7e2fbd6e9d3c5
Lluís <lluis.revilla@gmail.com>
2021-02-14 00:11:58 +0100

    Provide get_attachment

--- R/get_attachment.R
+++ R/get_attachment.R
@@ -1,21 +1,47 @@
-get_attachment <- function(issue, host) {
+#' Retrieve information about attachments
+#'
+#' Given an issue or an attachment
+#' @inheritParams get_comment
+#' @param attachment A numeric ID of the attachment.
+#' @return A data.frame with the information available.
+#' @export
+#' @examples
+#' get_attachment(issue = 1)
+#' get_attachment(attachment = 1)
+get_attachment <- function(issue, attachment, host) {
     host <- missing_host(host)
+    if (missing(issue) & missing(attachment)) {
+        stop("Both issue and attachment can't be missing", call. = FALSE)
+    }
+    if (!missing(issue) & !missing(attachment)) {
+        warning("Issue ID is ignored", call. = FALSE)
+    }
+    if (missing(issue)) {
+        attachment <- get_attachmenta(attachment, host)
+    } else {
+        attachment <- get_attachmenti(issue, host)
+    }
+    attachment$last_change_time <- time(attachment$last_change_time)
+    attachment$creation_time <- time(attachment$creation_time)
+    attachment
+}
+
+get_attachmenti <- function(issue, host) {
     url <- paste0(host, "rest/bug/", issue, "/attachment")
     attachments <- httr::GET(url, headers)
     attachments <- httr::content(attachments)
-    l <- lapply(attachments$bugs, function(x){
-        x[[1]][lengths(x[[1]]) == 0] <- list("")
-        as.data.frame(x[[1]])})
-    do.call(rbind, l)
+    attachments <- attachments$bugs[[1]]
+    for (i in seq_along(attachments)) {
+        x <- flatten_list(attachments[[i]])
+        attachments[[i]] <- x[, match(names(attachments[[i]]), colnames(x))]
+    }
+
+    do.call(rbind, attachments)
 }
 
-get_attachment <- function(attachment, host){
-    host <- missing_host(host)
+get_attachmenta <- function(attachment, host){
     url <- paste0(host, "rest/bug/attachment/", attachment)
     attachments <- httr::GET(url, headers)
     attachments <- httr::content(attachments)
-    l <- lapply(attachments$bugs, function(x){
-        x[[1]][lengths(x[[1]]) == 0] <- list("")
-        as.data.frame(x[[1]])})
-    do.call(rbind, l)
+    flatten_list(attachments$attachments[[1]])
 }
