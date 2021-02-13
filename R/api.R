e53da1e9701aee078c9103278d2392bd37fde488
Lluís <lluis.revilla@gmail.com>
2021-02-13 13:57:58 +0100

    Provide more useful information and control over authentication

--- R/api.R
+++ R/api.R
@@ -7,10 +7,12 @@ headers <- httr::add_headers("X-BUGZILLA-API-KEY" = Sys.getenv("R_BUGZILLA"),
 
 #' Authentication
 #'
-#' Obtain an API key or check if it is working.
+#' Obtain an API key or check if it can fine one that works for this host and
+#' the version of the bugzilla provided works with this package.
 #' @param host URL of the bugzilla instance if missing the R bugzilla is assumed.
 #' @param key API key to check.
 #' @return TRUE invisibly if the actions are performed.
+#' @seealso set_key() if you want to temporary use an API key.
 #' @rdname authentication
 #' @importFrom cli cli_ul
 #' @importFrom utils browseURL
@@ -18,9 +20,12 @@ headers <- httr::add_headers("X-BUGZILLA-API-KEY" = Sys.getenv("R_BUGZILLA"),
 create_bugzilla_key <- function(host) {
     host <- missing_host(host)
     url <- paste0(host, "userprefs.cgi?tab=apikey")
-
-    if (!nzchar(Sys.getenv("R_BUGZILLA"))) {
-        cli::cli_alert_warning("Already found an API key.")
+    if (check_api_version(host)) {
+        msg <- "This package hasn't been tested with this Bugzilla version."
+        cli::cli_alert_warning(msg)
+    }
+    if (set_key()) {
+        check_authentication(host = host)
         return(invisible(TRUE))
     }
     browseURL(url)
@@ -28,6 +33,24 @@ create_bugzilla_key <- function(host) {
              "\t{.code usethis::edit_r_environ()} might come handy")
     cli::cli_ul(paste0(msg, collapse = ""))
     cli::cli_ul("Restart R session so that changes take effect.")
+    cli::cli_alert_info("Or use {.code set_key} to use it only for this session.")
+    invisible(TRUE)
+}
+
+#' Set key to use
+#'
+#' If you don't want to have the API key on .Renviron or you want to use another
+#' API key for bugzilla you can set it up this way. It will store the key in the
+#' envirnoment which will only work for this R session.
+#' @param key The API key you want to use.
+#' @return TRUE
+#' @export
+set_key <- function(key = Sys.getenv("R_BUGZILLA")) {
+    if (!valid_key(key)) {
+        return(invisible(FALSE))
+    }
+    Sys.setenv("R_BUGZILLA" = key)
+    cli::cli_alert_success("Key set for this session.")
     invisible(TRUE)
 }
 
@@ -39,11 +62,11 @@ check_authentication <- function(key = Sys.getenv("R_BUGZILLA"), host) {
     httr::handle(host)
     whoami <- httr::GET(paste0(host, "rest/whoami"),
                         httr::add_headers("X-BUGZILLA-API-KEY" = key,
-                                          Application = "https://github.com/llrs/bugRzilla/"))
+                                          "User-agent" = "https://github.com/llrs/bugRzilla/"))
     if ("error" %in% names(httr::content(whoami))) {
-        cli::cli_alert_danger("Not authenticated.")
+        cli::cli_alert_danger("Not authenticated on this site.")
     }
-    cli::cli_alert_success("Authenticated!")
+    cli::cli_alert_success("Authenticated on this site!")
     invisible(TRUE)
 }
 
@@ -76,4 +99,13 @@ check_last_audit <- function(product, host) {
     audit <- httr::GET(paste0(host, "rest/last_audit_time"),
                          class = product)
     as.POSIXct(httr::content(audit)$last_audit_time)
+}
+
+
+valid_key <- function(key) {
+    if (key == "" & nchar(key) != 40) {
+        cli::cli_alert_danger("Key not valid or not set.")
+        return(FALSE)
+    }
+    TRUE
 }
