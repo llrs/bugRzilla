2d9a6442e8c932d98853df112b9792a4dfbe0313
Lluís <lluis.revilla@gmail.com>
2021-02-14 00:12:07 +0100

    Provide history of issues

--- R/bug_history.R
+++ R/bug_history.R
@@ -1,16 +1,26 @@
 #' Provide information about the history of an issue.
+#'
+#' See what has changed of an issue.
 #' @inheritParams get_issue
 #' @param new_since A character with a Date in YYYY-MM-DD format.
 #' @export
+#' @examples
+#' get_history(issue = 1)
 get_history <- function(issue, host, new_since = NULL) {
     host <- missing_host(host)
-    stopifnot(is.numeric(issue), issue > 0, length(issue) != 1)
+    stopifnot(is.numeric(issue), issue > 0, length(issue) == 1)
     url <- paste0(host, "rest/bug/", issue, "/history")
      if (is.null(new_since)) {
          history <- httr::GET(url, headers)
      } else {
-         history <- httr::GET(url, new_since = as.character(new_since), headers)
-
+         url <- paste(url, "?new_since=", new_since)
+         history <- httr::GET(url, headers)
     }
     history <- httr::content(history)
+    history <- history$bugs[[1]]$history
+    l <- lapply(history, flatten_list)
+    df <- do.call(rbind, l)
+    df$when <- time(df$when)
+    df$issue <- issue
+    df
 }
