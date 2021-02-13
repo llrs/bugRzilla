ee57c81380359a00645be36a0f28270670948a83
Lluís <lluis.revilla@gmail.com>
2021-02-13 12:13:24 +0100

    Fix assumption (not all issues have attachment)

--- R/parser.R
+++ R/parser.R
@@ -11,28 +11,35 @@ parse_issue <- function(xml) {
     names(values) <- names[!names %in% c("long_desc", "attachment")]
     # See who performed the action
     # Only the reporter and the assigned_to has name as attribute
-    who <- xml2::xml_attr(xml2[26:27], "name")
+    who <- xml2::xml_attr(xml2, "name")
+    who <- who[!is.na(who)]
     names(who) <- c("reported_by", "assigned_by")
     values <- c(values, who)
 
     long_desc <- xml2[names %in% "long_desc"]
     ld <- lapply(long_desc, parse_long_desc)
     comments <- as.data.frame(t(simplify2array(ld)))
-    attachment <- xml2[names %in% "attachment"]
-    at <- lapply(attachment, parse_attachment)
-    attachments <- as.data.frame(t(simplify2array(at)))
+
+    # Merge general information with comments
     values <- as.data.frame(as.list(values))
     out <- cbind.data.frame(comments, values)
-    out <- merge(out, attachments, all = TRUE, sort = FALSE,
-                 by = "attachid", suffixes = c(".comment", ".attachment"))
+
+    # Merge attachment information
+    if (any(names %in% "attachment")) {
+        attachment <- xml2[names %in% "attachment"]
+        at <- lapply(attachment, parse_attachment)
+        attachments <- as.data.frame(t(simplify2array(at)))
+        out <- merge(out, attachments, all = TRUE, sort = FALSE,
+                     by = "attachid", suffixes = c(".comment", ".attachment"))
+        out$delta_ts.comment <- as.POSIXct(out$delta_ts.comment)
+        out$delta_ts.attachment <- as.POSIXct(out$delta_ts.attachment)
+        out$date <- as.POSIXct(out$date)
+    }
     out$comment_count <- as.numeric(out$comment_count)
     out$commentid <- as.numeric(out$commentid)
     out$attachid <- as.numeric(out$attachid)
     out$bug_when <- as.POSIXct(out$bug_when)
     out$creation_ts <- as.POSIXct(out$creation_ts)
-    out$delta_ts.comment <- as.POSIXct(out$delta_ts.comment)
-    out$delta_ts.attachment <- as.POSIXct(out$delta_ts.attachment)
-    out$date <- as.POSIXct(out$date)
     out
 }
 
